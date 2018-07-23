7c7507ad45d71e395e131dea63e0f9eb0fca1e07
athanasiouconst <csiouath@gmail.com>
2018-07-23 19:55:25 +0300

    rest client calls create, update , delete

--- src/main/java/com/athanasiou/rest/PatientWSClient.java
+++ src/main/java/com/athanasiou/rest/PatientWSClient.java
@@ -5,6 +5,16 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.Response;
 
+/*
+create patient
+update patient
+delete patient
+through rest call to spring boot app
+*/
+
+
+
+
 
 public class PatientWSClient {
 
@@ -14,26 +24,37 @@ public class PatientWSClient {
 	public static void main(String[] args) {
 
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(PATIENT_SERVICE_URL).path(PATIENTS).path("/{id}").resolveTemplate("id", 157);
+
+
+		Patient newPatient = new Patient();
+		newPatient.setName("Constantinos Athanasiou");
+		WebTarget postTarget = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
+		Patient createdPatient = postTarget.request().post(Entity.entity(newPatient, MediaType.APPLICATION_XML),
+				Patient.class);
+		System.out.println("Created Patient ID " + createdPatient.getId());
+
+
+
+		WebTarget target = client.target(PATIENT_SERVICE_URL).path(PATIENTS).path("/{id}").resolveTemplate("id", 160);
 		Invocation.Builder request = target.request();
 		Patient patient = request.get(Patient.class);
 		System.out.println(patient.getId());
 		System.out.println(patient.getName());
-
-		patient.setName("Constantinos Athanasiou");
-
+		patient.setName("Michail Konstantina!");
 		WebTarget putTarget = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
 		Response updateResponse = putTarget.request().put(Entity.entity(patient, MediaType.APPLICATION_XML));
 		System.out.println(updateResponse.getStatus());
 		updateResponse.close();
 
-		Patient newPatient = new Patient();
-		newPatient.setName("Michail Konstantina");
 
-		WebTarget postTarget = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
-		Patient createdPatient = postTarget.request().post(Entity.entity(patient, MediaType.APPLICATION_XML),
-				Patient.class);
-		System.out.println("Created Patient ID " + createdPatient.getId());
+		WebTarget targetDelete = client.target(PATIENT_SERVICE_URL).path(PATIENTS).path("/{id}").resolveTemplate("id", 158);
+		Invocation.Builder requestDelete = targetDelete.request();
+		Patient patientDelete = requestDelete.get(Patient.class);
+		System.out.println(patientDelete.getId());
+		System.out.println(patientDelete.getName());
+		Response deleteResponse = targetDelete.request().delete();
+		System.out.println(deleteResponse.getStatus());
+		deleteResponse.close();
 
 		client.close();
 
---
+++ src/test/java/com/athanasiou/rest/RestwsclientApplicationTests.java
@@ -0,0 +1,19 @@
+package com.athanasiou.rest;
+
+/**
+ * Created by costas on 23/7/2018.
+ */
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestwsclientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
